9ef20a57b53236eea7cbcffdd0a99db04d346970
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-25 15:43:40 +0100

    Implement routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navs/NavBar';
 import './Sass/App.scss';
 import links from './utils/links';
@@ -6,6 +7,13 @@ function App() {
   return (
     <div className="App">
       <NavBar links={links} />
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="work" element={<h1>Work</h1>} />
+        <Route path="blog" element={<h1>Blog</h1>} />
+        <Route path="contact" element={<h1>Contact</h1>} />
+      </Routes>
     </div>
   );
 }
