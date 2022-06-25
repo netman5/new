ec0f99b7fda3ff7a17442c74fe209144d8a73d7c
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-25 15:41:41 +0100

    Fixed padding issue on the body

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import NavBar from './components/Navs/NavBar';
 import './Sass/App.scss';
+import links from './utils/links';
 
 function App() {
-  const links = [
-    { id: '1', path: '/', name: 'Home' },
-    { id: '2', path: '/work', name: 'Work' },
-    { id: '3', path: '/contact', name: 'Contact' },
-  ];
-
   return (
     <div className="App">
       <NavBar links={links} />
