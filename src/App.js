b3c4bacfc1b291cde5d5677c8e56853d6a9f8334
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-26 16:09:43 +0100

    Add active path to routes & a little transition

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="work" element={<h1>Work</h1>} />
-        <Route path="blog" element={<h1>Blog</h1>} />
-        <Route path="contact" element={<h1>Contact</h1>} />
+        <Route path="/work" element={<h1>Work</h1>} />
+        <Route path="/blog" element={<h1>Blog</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
       </Routes>
     </div>
   );
