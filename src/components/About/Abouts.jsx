0fa6a99e53a4b39eaef53fc46af0ac75ac6d3674
Ola Ishola <olaishola@hotmail.co.uk>
2022-07-01 10:49:24 +0100

    Add tech icons to about section

--- src/components/About/Abouts.jsx
+++ src/components/About/Abouts.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import {
   SiJavascript, SiRedux, SiHtml5, SiCss3,
+  SiMaterialui, SiTailwindcss, SiTypescript, SiRubyonrails,
+  SiHeroku, SiVercel, SiVisualstudiocode, SiNetlify,
 } from 'react-icons/si';
-import { FaReact, FaSass } from 'react-icons/fa';
 import {
-  DiResponsive, DiNodejs, DiMongodb, DiGit,
+  FaReact, FaSass, FaBootstrap, FaNpm, FaServer,
+} from 'react-icons/fa';
+import {
+  DiResponsive, DiNodejs, DiMongodb, DiGit, DiRuby, DiPostgresql,
 } from 'react-icons/di';
+
+import { AiFillApi } from 'react-icons/ai';
+import {
+  BsGithub, BsFillTerminalFill, BsTools,
+} from 'react-icons/bs';
+
+import { BiCodeAlt } from 'react-icons/bi';
 
 const Abouts = () => (
   <section className="about">
-    <h2>About</h2>
+    <h2>About & Tech Stacks</h2>
     <div className="about-container">
       <div className="about-container__skills">
         <div className="about-container__skills__item">
-          <h3 className="about-container__skills__item__title">Frontend</h3>
+          <div className="about-container__skills__top">
+            <h3 className="about-container__skills__item__title">Frontend</h3>
+            <BiCodeAlt className="about-container__skills__item_top__icon" />
+          </div>
           <p>Experienced building frontend application with following technologies</p>
           <ul className="about-container__skills__item__list">
             <li className="about-container__skills__item__list__item">
@@ -24,18 +38,47 @@ const Abouts = () => (
               <SiJavascript className="about-container__skills__item__list__item__icon" />
               <FaReact className="about-container__skills__item__list__item__icon" />
               <SiRedux className="about-container__skills__item__list__item__icon" />
-              <DiGit className="about-container__skills__item__list__item__icon" />
+              <SiMaterialui className="about-container__skills__item__list__item__icon" />
+              <SiTailwindcss className="about-container__skills__item__list__item__icon" />
+              <FaBootstrap className="about-container__skills__item__list__item__icon" />
+              <SiTypescript className="about-container__skills__item__list__item__icon" />
             </li>
           </ul>
         </div>
 
         <div className="about-container__skills__item">
-          <h3 className="about-container__skills__item__title">Backend</h3>
-          <p>Experienced building backend APIs and services with</p>
+          <div className="about-container__skills__top">
+            <h3 className="about-container__skills__item__title">Backend</h3>
+            <FaServer className="about-container__skills__item_top__icon" />
+          </div>
+          <p>Experienced building backend APIs and services with below techs</p>
           <ul className="about-container__skills__item__list">
             <li className="about-container__skills__item__list__item">
               <DiNodejs className="about-container__skills__item__list__item__icon" />
+              <DiRuby className="about-container__skills__item__list__item__icon" />
+              <SiRubyonrails className="about-container__skills__item__list__item__icon" />
               <DiMongodb className="about-container__skills__item__list__item__icon" />
+              <DiPostgresql className="about-container__skills__item__list__item__icon" />
+              <AiFillApi className="about-container__skills__item__list__item__icon" />
+            </li>
+          </ul>
+        </div>
+
+        <div className="about-container__skills__item">
+          <div className="about-container__skills__top">
+            <h3 className="about-container__skills__item__title">Tools</h3>
+            <BsTools className="about-container__skills__item_top__icon" />
+          </div>
+          <ul className="about-container__skills__item__list">
+            <li className="about-container__skills__item__list__item">
+              <DiGit className="about-container__skills__item__list__item__icon" />
+              <BsGithub className="about-container__skills__item__list__item__icon" />
+              <BsFillTerminalFill className="about-container__skills__item__list__item__icon" />
+              <SiHeroku className="about-container__skills__item__list__item__icon" />
+              <SiVercel className="about-container__skills__item__list__item__icon" />
+              <SiVisualstudiocode className="about-container__skills__item__list__item__icon" />
+              <SiNetlify className="about-container__skills__item__list__item__icon" />
+              <FaNpm className="about-container__skills__item__list__item__icon" />
             </li>
           </ul>
         </div>
