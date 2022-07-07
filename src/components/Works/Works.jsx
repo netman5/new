aa2f2b7adc3a89b5cbfa384cd56e2b0739b2d88f
Ola Ishola <olaishola@hotmail.co.uk>
2022-07-07 14:29:34 +0100

    Complete Styling the work section

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -26,30 +26,30 @@ const Works = () => {
     <div className="work-container">
       <div className="work-container__contents">
         <div className="work-container__contents__title">
-          <h2>My Works</h2>
+          <h2>My recent Works</h2>
         </div>
         <div className="work-content__items">
           {data.map(({ id, attributes }) => (
             <div key={id} className="work-content__item">
+              <h3>{attributes.name}</h3>
               <div className="content__item__top">
-                <h3>{attributes.name}</h3>
-                <div>
-                  <span>{attributes.project_details.type}</span>
-                  <span>{attributes.project_details.role}</span>
-                  <span>{attributes.project_details.year}</span>
+                <div className="content__item__top__container">
+                  <div>
+                    <h5>{attributes.project_details.role}</h5>
+                  </div>
+                  <div className="stacks-lists">
+                    {attributes.tech.stacks.map((item) => <ul key={item}><li>{item}</li></ul>)}
+                  </div>
                 </div>
               </div>
-              <div className="stacks-lists">
-                {attributes.tech.stacks.map((item) => <ul key={item}><li>{item}</li></ul>)}
-              </div>
               <div className="work-content__item-image">
-                <img src={attributes.img_url} alt={attributes.name} style={{ width: '10%' }} />
+                <img src={attributes.img_url} alt={attributes.name} />
               </div>
               <div>
                 <div className="work-content__item-description">{attributes.descriptions}</div>
-                <div>
-                  <a href={attributes.live_url} target="_blank" rel="noopener noreferrer">See here</a>
-                  <a href={attributes.source_Url} target="_blank" rel="noopener noreferrer">Source</a>
+                <div className="work-content__item-projects__links">
+                  <a href={attributes.live_Url} target="_blank" rel="noopener noreferrer" className="live">See here</a>
+                  <a href={attributes.source_Url} target="_blank" rel="noopener noreferrer" className="repo">Source</a>
                 </div>
               </div>
             </div>
